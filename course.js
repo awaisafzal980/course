146b838b3f1ff1426232c294e37d245e150ad20c
awaisafzal980 <awaisafzal3232@gmail.com>
2020-05-03 16:34:02 +0500

    index.html &&  course.js && k.jpg && kl.jpg && style.css

---
+++ course.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  $('.view').click(function(){
+    $('.details').addClass('active')
+  });
+  $('.close').click(function(){
+    $('.details').removeClass('active')
+  })
+})
