37d89429cd9e11c5fb45a633ca5d661cbea69387
awaisafzal980 <awaisafzal3232@gmail.com>
2020-06-14 03:17:27 -0700

    index.html && style.css

--- course.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-  $('.view').click(function(){
-    $('.details').addClass('active')
-  });
-  $('.close').click(function(){
-    $('.details').removeClass('active')
-  })
-})
